3df3af268cdb502953a241b3873241a0111af8af
Batou <jjdormevil@gmail.com>
2019-06-18 01:46:32 +0200

    initial commit

---
+++ src/test/java/com/flocondria/boisjoly/LilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flocondria.boisjoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
