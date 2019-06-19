ee8d12110685c5b1b1dc47e94e826847bb4a4b66
Batou <jjdormevil@gmail.com>
2019-06-19 14:44:48 +0200

    Fixe NoSuchFileException

--- src/main/java/com/flocondria/boisjoly/context/template/infrastructure/FileHandler.java
+++ src/main/java/com/flocondria/boisjoly/context/template/infrastructure/FileHandler.java
@@ -2,7 +2,9 @@ package com.flocondria.boisjoly.context.template.infrastructure;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.codec.multipart.FilePart;
+import org.apache.commons.io.FileUtils;
 import org.springframework.http.codec.multipart.Part;
 import org.springframework.web.reactive.function.BodyExtractors;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -11,6 +13,11 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 
 @Configuration
@@ -24,10 +31,27 @@ public class FileHandler {
                 .body(BodyExtractors.toMultipartData())
                 .flatMap(part -> {
                     Map<String, Part> map = part.toSingleValueMap();
-                    FilePart filepart = (FilePart) map.get("files");
-                    return filepart
-                            .transferTo(new File(dir + "/" + filepart.filename()).toPath())
-                            .then(ServerResponse.ok().body(BodyInserters.fromObject("Ok")));
+                    Part filepart = map.get("files");
+
+                    try {
+                        File realFile = new File("./uploaded/" + filepart.headers().getContentDisposition().getFilename());
+                        FileUtils.touch(realFile);
+                        WritableByteChannel channel = Files.newByteChannel(realFile.toPath(), StandardOpenOption.WRITE);
+
+                        return DataBufferUtils.write(filepart.content(), channel)
+                                .map(DataBufferUtils::release)
+                                .doOnTerminate(() -> {
+                                    try {
+                                        channel.close();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                })
+                                .then(ServerResponse.ok().body(BodyInserters.fromObject("Ok")));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return ServerResponse.ok().body(BodyInserters.fromObject("Not Ok"));
                 });
     }
 }
